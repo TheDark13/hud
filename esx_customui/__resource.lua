d3f74fc47d9d4b9e9ea685be273ffe82c5542d2c
TheDark13 <77801573+TheDark13@users.noreply.github.com>
2021-07-16 19:55:54 +0200

    Add files via upload

---
+++ esx_customui/__resource.lua
@@ -0,0 +1,92 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'FiveM Custom UI for ESX'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+files {
+	-- Main Images
+	'html/ui.html',
+	'html/style.css',
+	'html/grid.css',
+	'html/main.js',
+	-- Money Images
+	'html/img/credit-card.png',
+	'html/img/money-bag.png',
+	'html/img/wallet.png',
+	-- Job Images
+	'html/img/jobs/ltd.png',
+	'html/img/jobs/unicorn.png',
+	'html/img/jobs/fishing.png',
+	'html/img/jobs/cardealer.png',
+	'html/img/jobs/journaliste.png',
+	'html/img/jobs/ambulance.png',
+	'html/img/jobs/families.png',
+	'html/img/jobs/fisherman.png',
+	'html/img/jobs/tattoo.png',
+	'html/img/jobs/lumberjack.png',
+	'html/img/jobs/weazelnews.png',
+	'html/img/jobs/fueler.png',
+	'html/img/jobs/vagos.png',
+	'html/img/jobs/cartel.png',
+	'html/img/jobs/biker.png',
+	'html/img/jobs/fire.png',
+	'html/img/jobs/slaughterer.png',
+	'html/img/jobs/mecano.png',
+	'html/img/jobs/tailer.png',
+	'html/img/jobs/miner.png',
+	'html/img/jobs/surenos.png',
+	'html/img/jobs/police.png',
+	'html/img/jobs/realestateagent.png',
+	'html/img/jobs/securoserv.png',
+	'html/img/jobs/sheriff.png',
+	'html/img/jobs/swat.png',
+	'html/img/jobs/fueler.png',
+	'html/img/jobs/thelost.png',
+	'html/img/jobs/taxi.png',
+	'html/img/jobs/marabunta.png',
+	'html/img/jobs/deliverer.png',
+	'html/img/jobs/speedrap.png',
+	'html/img/jobs/unemployed.png',
+	'html/img/jobs/ammu.png',
+	'html/img/jobs/barber.png',
+	'html/img/jobs/ballas.png',
+	'html/img/jobs/avocat.png',
+	'html/img/jobs/motorcycle.png',
+	'html/img/jobs/burgershot.png',
+	'html/img/jobs/illegal.png',
+	'html/img/jobs/banker.png',
+	'html/img/jobs/ms13.png',
+	'html/img/jobs/blanchisseur.png',
+	'html/img/jobs/ammunation.png',
+	'html/img/jobs/offambulance.png',
+	'html/img/jobs/offpolice.png',
+	'html/img/jobs/gouvernement.png',
+	'html/img/jobs/tabac.png',
+	'html/img/jobs/unicorn.png',
+	'html/img/jobs/vigne.png',
+	'html/img/jobs/unemployed2.png',
+	-- Vehicle Images
+	--'html/img/vehicle/damage.png',
+	--'html/img/vehicle/gas.png',
+	--'html/img/vehicle/speed.png',
+	--'html/img/vehicle/locked.png',
+	--'html/img/vehicle/unlocked.png',
+	-- Bottom Round Images
+	'html/img/hunger.png',
+	'html/img/water.png',
+	'html/img/drunk.png',
+	'html/img/speaker1.png',
+	'html/img/speaker2.png',
+	'html/img/speaker3.png',
+	'html/img/backpack.png'
+
+}
